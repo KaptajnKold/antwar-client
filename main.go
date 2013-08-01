317f7ad310992b6cc7c895932d35040be2712ba0
Adam Lett <adamlett@gmail.com>
2013-08-01 13:17:49 +0200

    Remove obsolete ant races

--- main.go
+++ main.go
@@ -2,18 +2,14 @@ package main
 
 import (
 	"github.com/KaptajnKold/antwar"
-	"github.com/KaptajnKold/clever_ant"
 	"github.com/KaptajnKold/ghengis"
 	"github.com/KaptajnKold/naive_ant"
-	"github.com/KaptajnKold/random_ant"
 )
 
 func main() {
 	antwar.NewGame([]*antwar.Team{
 		antwar.NewTeam("ghengis", ghengis.Spawn),
 		antwar.NewTeam("ghengis2", ghengis.Spawn),
-		antwar.NewTeam("random", random_ant.Spawn),
 		antwar.NewTeam("naive", naive_ant.Spawn),
-		antwar.NewTeam("clever", clever_ant.Spawn),
 	})
 }
